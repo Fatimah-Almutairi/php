8585390641de98ce831f22b406ebb7e9918ec351
fatimah06 <fatima.m.mutairi@gmail.com>
2022-06-27 23:24:09 +0300

    php

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
    $a = 5;
    $b = 6;
    $c = $a + $b;
-   
+   $d = $a - $b;
+
+   $b -= 3;
+   echo $b;
+   echo "<br> <br>";
+   $c += $a;
+   echo $c;
+
+   echo "<br>";
    $first_name = 'Fatimah';
    $last_name = 'Maohammed';
 
