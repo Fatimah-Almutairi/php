57cd4d03ec6bf2e5daf596f7597cba3f5cbb988a
fatimah06 <fatima.m.mutairi@gmail.com>
2022-06-28 22:53:45 +0300

    php

--- index.php
+++ index.php
@@ -1,25 +1,70 @@
 
    <?php 
-   
-   $birth_month = 13;
 
-   if($birth_month == 2 || $birth_month == 3 || $birth_month == 4){
-       echo " Your birth is on Fall";
+     $month_Number = 123;
 
-   }elseif($birth_month == 5 || $birth_month == 6 || $birth_month == 7){
+     switch($month_Number){
+         case 1:
+            echo "January";
+        break;
+        case 2:
+            echo "Feb";
+        break;
+        case 3:
+            echo "Mar";
+        break;
+        case 4:
+            echo "Apr";
+        break;
+        case 5:
+            echo "May";
+        break;
+        case 6:
+            echo "Jun";
+        break;
+        case 7:
+            echo "Jul";
+        break;
+        case 8:
+            echo "Agu";
+        break;
+        case 9:
+            echo "Spt";
+        break;
+        case 10:
+            echo "Oct";
+        break;
+        case 11:
+            echo "Nov";
+        break;
+        case 12:
+            echo "Dec";
+        break;
+        default:
+            echo "Check for the correct Month Number";
+        break;
+        
+     }
 
-    echo " Your birth in on Spring ";
-   }elseif($birth_month == 8 || $birth_month == 9 || $birth_month == 10){
-    echo " Your birth in on Summer ";
+//    $birth_month = 13;
+
+//    if($birth_month == 2 || $birth_month == 3 || $birth_month == 4){
+//        echo " Your birth is on Fall";
+
+//    }elseif($birth_month == 5 || $birth_month == 6 || $birth_month == 7){
+
+//     echo " Your birth in on Spring ";
+//    }elseif($birth_month == 8 || $birth_month == 9 || $birth_month == 10){
+//     echo " Your birth in on Summer ";
 
 
-   }elseif($birth_month == 11 || $birth_month == 12 || $birth_month == 1){
+//    }elseif($birth_month == 11 || $birth_month == 12 || $birth_month == 1){
 
-    echo " Your birth in on Winter ";
+//     echo " Your birth in on Winter ";
 
-   }else{
-       echo " check your Birth Month";
-   }
+//    }else{
+//        echo " check your Birth Month";
+//    }
 //    $a = 5;
 //    $b = 6;
 //    $c = $a + $b;
