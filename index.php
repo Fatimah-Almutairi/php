78729605c02c57e4380c3ebb1e1ebc9e38bad071
fatimah06 <fatima.m.mutairi@gmail.com>
2022-06-28 22:17:42 +0300

    php

--- index.php
+++ index.php
@@ -1,31 +1,50 @@
 
    <?php 
    
-   $a = 5;
-   $b = 6;
-   $c = $a + $b;
-   $d = $a - $b;
+   $birth_month = 13;
 
-   $b -= 3;
-//    echo $b;
-   echo "<br>";
-   $c += $a;
-   echo $c;
+   if($birth_month == 2 || $birth_month == 3 || $birth_month == 4){
+       echo " Your birth is on Fall";
 
-   echo "<br>";
-   $first_name = 'Fatimah';
-   $last_name = 'Maohammed';
+   }elseif($birth_month == 5 || $birth_month == 6 || $birth_month == 7){
 
-   $full_name = $first_name . " " . $last_name;
+    echo " Your birth in on Spring ";
+   }elseif($birth_month == 8 || $birth_month == 9 || $birth_month == 10){
+    echo " Your birth in on Summer ";
 
-   echo $full_name;
 
-   echo "<br>";
-   echo "<br>";
-   echo "<br>";
-   if($a < $b ){
-       echo "A is smaller than B";
+   }elseif($birth_month == 11 || $birth_month == 12 || $birth_month == 1){
+
+    echo " Your birth in on Winter ";
+
    }else{
-       echo "Else";
+       echo " check your Birth Month";
    }
+//    $a = 5;
+//    $b = 6;
+//    $c = $a + $b;
+//    $d = $a - $b;
+
+//    $b -= 3;
+// //    echo $b;
+//    echo "<br>";
+//    $c += $a;
+//    echo $c;
+
+//    echo "<br>";
+//    $first_name = 'Fatimah';
+//    $last_name = 'Maohammed';
+
+//    $full_name = $first_name . " " . $last_name;
+
+//    echo $full_name;
+
+//    echo "<br>";
+//    echo "<br>";
+//    echo "<br>";
+//    if($a < $b ){
+//        echo "A is smaller than B";
+//    }else{
+//        echo "Else";
+//    }
     ?>
