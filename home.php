1ab598c3fc8ab0e4b1ea0d5b44cb5ae0632f6e04
fatimah06 <fatima.m.mutairi@gmail.com>
2022-07-01 00:01:58 +0300

    php

--- home.php
+++ home.php
@@ -8,4 +8,9 @@
     <input type="text" value="height" name= "height" placeholder="Enter your height"><br>
     <input type="radio" id="male" name="male" value="male">
   <label>Male</label><br>
+  <input type="radio" name="female" value= "female">
+  <label>Female</label><br>
+  <input type="submit" name="button" value="submit">
+  <form>
+</body>
 </html>
