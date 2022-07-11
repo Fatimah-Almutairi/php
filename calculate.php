df68a9b92d16e0e751a46db994be6bace5418f2d
fatimah06 <fatima.m.mutairi@gmail.com>
2022-07-11 22:40:03 +0300

    php

--- calculate.php
+++ calculate.php
@@ -4,6 +4,7 @@ $no1 = $_POST['no1'];
 //  echo"<br>";
 $no2 = $_POST['no2'];
 // echo"<br> <br>";
+$arthematic_operation = $_POST['arth_operation'];
 $cal_val = $no1 + $no2;
-echo $cal_val;
+echo $arthematic_operation;
 ?>
