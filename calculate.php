dbef008852f0c7a0ac95f0caf886d490dece1fb2
fatimah06 <fatima.m.mutairi@gmail.com>
2022-07-14 23:54:33 +0300

    php

--- calculate.php
+++ calculate.php
@@ -5,7 +5,11 @@ $no1 = $_POST['no1'];
 $no2 = $_POST['no2'];
 // echo"<br> <br>";
 $arthematic_operation = $_POST['arth_operation'];
-if($arthematic_operation == 'sum_operation')
+if($arthematic_operation == 'sum_operation') {
+    $cal_val = $no1 + $no2;
+}else if($arthematic_operation == 'sub_operation') {
+    $cal_val = $no1 + $no2;
+}
 $cal_val = $no1 + $no2;
 echo $arthematic_operation;
 ?>
