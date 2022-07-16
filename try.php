44d03081b2abb65075aba5aac3a6603fe2f28115
fatimah06 <fatima.m.mutairi@gmail.com>
2022-07-16 23:46:56 +0300

    php

--- try.php
+++ try.php
@@ -3,14 +3,15 @@
  $y =5;
  $z = cal($x, $y);
 
-echo nw('Ali');
+// echo nw('Ali');
 
 
-function nw($name){
-    echo "This is first function " . $name;
-}
+// function nw($name){
+//     echo "This is first function " . $name;
+// }
 
 function cal($x, $y){
-    
+    $z = $x + $y;
+    echo $z;
 }
 ?>
