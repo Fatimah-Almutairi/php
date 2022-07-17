17a0e07607c415419e99b2bb6d3d38827c766e3d
fatimah06 <fatima.m.mutairi@gmail.com>
2022-07-17 13:18:33 +0300

    php

--- try.php
+++ try.php
@@ -1,8 +1,8 @@
 <?php
  $x =2;
  $y =5;
- rec_area($x, $y);
-
+ $area = cal ( $x , $y ) * 2;
+ echo $area;
 // echo nw('Ali');
 
 
@@ -12,14 +12,14 @@
 
 function cal($x, $y){
     $z = $x + $y;
-    echo $z;
+    return $z;
 }
 echo "<br>";
 
-function rec_area($x, $y){
-    $area = ($x+ $y) * 2;
-    echo $area;
-}
+// function rec_area($x, $y){
+//     $area = ( $x + $y ) * 2;
+//     echo $area;
+// }
 
 
 
