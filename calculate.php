ed61aa6c648057575ec81e6e7f86063d9f9325a0
fatimah06 <fatima.m.mutairi@gmail.com>
2022-07-19 22:23:01 +0300

    php

--- calculate.php
+++ calculate.php
@@ -11,39 +11,45 @@ if(isset($_POST['no1']) && isset($_POST['no2']) && !empty($_POST['no1']) && !emp
     $arthematic_operation = $_POST['arth_operation'];
     echo $arthematic_operation . "<br>";
 
+    if (is_int($no1) && is_int($no2)){
+
+    
 
 
-    // Using if Condition
+        // Using if Condition
 
-    // if($arthematic_operation == 'sum_operation') {
-    //     $cal_val = $no1 + $no2;
-    // }else if($arthematic_operation == 'sub_operation') {
-    //     $cal_val = $no1 - $no2;
-    // }else if($arthematic_operation == 'multiple_operation') {
-    //     $cal_val = $no1 * $no2;
-    // }else {
-    //     $cal_val = $no1 / $no2;
-    // }
+        // if($arthematic_operation == 'sum_operation') {
+        //     $cal_val = $no1 + $no2;
+        // }else if($arthematic_operation == 'sub_operation') {
+        //     $cal_val = $no1 - $no2;
+        // }else if($arthematic_operation == 'multiple_operation') {
+        //     $cal_val = $no1 * $no2;
+        // }else {
+        //     $cal_val = $no1 / $no2;
+        // }
 
 
 
-    switch ($arthematic_operation){
-        case 'sum_operation': 
-            $cal_val = $no1 + $no2;
+        switch ($arthematic_operation){
+            case 'sum_operation': 
+                $cal_val = $no1 + $no2;
+                break;
+            case 'sub_operation':
+                $cal_val = $no1 - $no2;
+                break;
+            case 'multiple_operation' :
+                $cal_val = $no1 * $no2;
+                break;
+            default:
+            $cal_val = $no1 / $no2;
             break;
-        case 'sub_operation':
-            $cal_val = $no1 - $no2;
-            break;
-        case 'multiple_operation' :
-            $cal_val = $no1 * $no2;
-            break;
-        default:
-        $cal_val = $no1 / $no2;
-        break;
 
+        }
+
+        echo $cal_val;
+    }else{
+        echo "You Should Enter Numeric Values";
     }
-
-    echo $cal_val;
 
 }else{
     echo 'Please enter your number.';
