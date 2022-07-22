c92955a99fc95312feacfb87768090e1513755ee
fatimah06 <fatima.m.mutairi@gmail.com>
2022-07-22 22:34:47 +0300

    php

--- homepage.php
+++ homepage.php
@@ -1,6 +1,9 @@
-<?php include('header.php'); ?>
-
+<!-- هنا ناديت الهيدر للموقع عشان مااعيد كتابته لكل صفحه لحال اسهل بالتعديل وارتب  -->
+<?php include('header.php'); ?>  
+<!-- بعدين هنا ابدا اكتب محتويات الصفحه هذي لحال  -->
 <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw0jLHUW0c0DP32-Zp7iyCpUK98UaMv-3tgg&usqp=CAU" style = "height : 300px;">
 
 <h2> Here Home Page</h2>
+
+<!-- هنا الفوتر للصفحات اربطه بالطريقه هذي اسهل كتابة وتعديل  -->
 <?php include('footer.php'); ?>
